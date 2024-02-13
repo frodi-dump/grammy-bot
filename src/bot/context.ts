7d2c16451b8b70999866d30c4c3a577e03c9322a
Frodi1998 <rafael.abdulov.650@gmail.com>
2024-02-13 20:45:41 +0500

    feat: add MyContext type and contextConstructor

---
+++ src/bot/context.ts
@@ -0,0 +1,31 @@
+import { Api, Context, SessionFlavor } from 'grammy';
+import { Update, UserFromGetMe } from 'grammy/types';
+import { Logger } from 'logger';
+
+export type SessionData = {
+  // field?: string;
+};
+
+type ExtendedContextFlavor = {
+  logger: Logger;
+};
+
+export type MyContext = Context & SessionFlavor<SessionData> & ExtendedContextFlavor;
+
+interface Dependencies {
+  logger: Logger;
+}
+
+export function createContextConstructor({ logger }: Dependencies) {
+  return class extends Context implements ExtendedContextFlavor {
+    logger: Logger;
+
+    constructor(update: Update, api: Api, me: UserFromGetMe) {
+      super(update, api, me);
+
+      this.logger = logger.child({
+        update_id: this.update.update_id,
+      });
+    }
+  } as unknown as new (update: Update, api: Api, me: UserFromGetMe) => MyContext;
+}
