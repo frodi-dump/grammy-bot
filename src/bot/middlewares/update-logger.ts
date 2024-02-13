e3be5de3ebcf7f43fe969037d4d5dc5f73b9f15c
Frodi1998 <rafael.abdulov.650@gmail.com>
2024-02-13 20:46:21 +0500

    feat: add middlewares

---
+++ src/bot/middlewares/update-logger.ts
@@ -0,0 +1,34 @@
+import { performance } from 'node:perf_hooks';
+import { Middleware } from 'grammy';
+import type { MyContext } from 'bot/context';
+import { getUpdateInfo } from 'bot/helpers';
+
+export function updateLogger(): Middleware<MyContext> {
+  return async (ctx, next) => {
+    ctx.api.config.use((previous, method, payload, signal) => {
+      ctx.logger.debug({
+        msg: 'bot api call',
+        method,
+        payload,
+      });
+
+      return previous(method, payload, signal);
+    });
+
+    ctx.logger.debug({
+      msg: 'update received',
+      update: getUpdateInfo(ctx),
+    });
+
+    const startTime = performance.now();
+    try {
+      await next();
+    } finally {
+      const endTime = performance.now();
+      ctx.logger.debug({
+        msg: 'update processed',
+        duration: endTime - startTime,
+      });
+    }
+  };
+}
